ce03fa3405220f71f8ce5d2640645a2645fb5bd0
puxxbu <puxxbu@users.noreply.github.com>
2022-09-09 19:36:56 +0700

    add logout function

--- TUBES_PBP/app/src/main/java/com/example/tubes_pbp/fragments/AkunFragment.kt
+++ TUBES_PBP/app/src/main/java/com/example/tubes_pbp/fragments/AkunFragment.kt
@@ -1,11 +1,14 @@
 package com.example.tubes_pbp.fragments
 
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import com.example.tubes_pbp.R
+import kotlinx.android.synthetic.main.fragment_akun.view.*
 
 class AkunFragment : Fragment(R.layout.fragment_akun) {
     override fun onCreateView(
@@ -13,7 +16,16 @@ class AkunFragment : Fragment(R.layout.fragment_akun) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_akun, container, false)
+        var dialog = LogoutAlert()
+        val rootView: View = inflater.inflate(R.layout.fragment_akun, container, false)
+
+        rootView.btnLogout.setOnClickListener {
+            dialog.show(parentFragmentManager, "alertLogout" )
+        }
+
+
+
+        return rootView
     }
 
 }
