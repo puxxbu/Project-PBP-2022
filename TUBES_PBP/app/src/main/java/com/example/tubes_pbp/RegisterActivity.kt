575caf5c13b6ff2e37eee1b1e04f318fdfa96d2a
puxxbu <puxxbu@users.noreply.github.com>
2022-09-21 20:52:58 +0700

    fix viewbinding

--- TUBES_PBP/app/src/main/java/com/example/tubes_pbp/RegisterActivity.kt
+++ TUBES_PBP/app/src/main/java/com/example/tubes_pbp/RegisterActivity.kt
@@ -1,9 +1,11 @@
 package com.example.tubes_pbp
 
 import android.app.DatePickerDialog
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.core.view.isEmpty
@@ -23,128 +25,134 @@ import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var usersDb :UsersDB
+    private lateinit var usersDb: UsersDB
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
-        binding.regBtnRegister.setOnClickListener(){
-            binding.tilNamaLengkap.editText
-            binding.tilTglLahir.editText
-            binding.tilNoHP.editText
-            binding.tilEmail.editText
-            binding.tilUsername.editText
-            binding.tilPassword.editText
-            binding.tietTglLahir.isTextInputLayoutFocusedRectEnabled
-        }
+        binding.tietTglLahir.setFocusable(false)
+
         val myCalendar = Calendar.getInstance()
 
         val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
             myCalendar.set(Calendar.YEAR, year)
             myCalendar.set(Calendar.MONTH, month)
             myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            updateLable(myCalendar)
-        }
-        tiet_tglLahir.setOnClickListener {
-            DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+            updateLable(myCalendar, binding)
         }
 
-        btnBackLoginListener()
-        btnRegisterListener()
-    }
+        binding.tietTglLahir.setOnClickListener {
+            DatePickerDialog(
+                this,
+                datePicker,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
 
-    private fun btnRegisterListener() {
-        reg_btnRegister.setOnClickListener(View.OnClickListener {
-            var checkRegister = false
-            resetAlert()
 
-            til_namaLengkap.editText?.text.toString()
-            til_tglLahir.editText?.text.toString()
-            til_noHP.editText?.text.toString()
-            til_email.editText?.text.toString()
-            til_username.editText?.text.toString()
-            til_password.editText?.text.toString()
-
-            val mBundle = Bundle()
-
-            mBundle.putString("username", til_username.toString())
-            mBundle.putString("password", til_password.toString())
-
-            if (til_username.toString().isEmpty()){
-                til_username.error = "Username must be filled "
-                checkRegister = false
-            }
-
-            if (til_password.isEmpty()){
-                til_password.error = "Password must be filled "
-                checkRegister = false
-            }
-
-            if (til_namaLengkap.isEmpty()){
-                til_namaLengkap.error = "Nama must be filled "
-                checkRegister = false
-            }
-
-            if (til_noHP.isEmpty()){
-                til_noHP.error = "Nomor HP must be filled "
-                checkRegister = false
-            }
-
-            if (til_email.isEmpty()){
-                til_email.error = "Email must be filled "
-                checkRegister = false
-            }
-
-            if (til_tglLahir.isEmpty()){
-                til_tglLahir.error = "Tanggal Lahir must be filled "
-                checkRegister = false
-            }
-
-            if(!til_namaLengkap.isEmpty() && !til_tglLahir.isEmpty() && !til_noHP.isEmpty() && !til_email.isEmpty() && !til_username.isEmpty() && !til_password.isEmpty() ){
-                checkRegister = true
-            }
-
-            if(!checkRegister){
-                return@OnClickListener
-
-            // kalo di kasih komen dari depan else line 113 sampe 118 gabakal item kalo btnregisnya di klik
-            }/*else{
-//                val moveLogin = Intent(this, LoginActivity::class.java)
-
-                val user = Users(0,til_username.toString(),til_password.toString(),til_namaLengkap.toString(),til_email.toString(),til_noHP.toString(),til_tglLahir.toString())
-                GlobalScope.launch(Dispatchers.IO){
-                    usersDb.usersDao().addUsers(user)*/
-            //}
-            Toast.makeText(this,"Masukkan berhasil!",Toast.LENGTH_SHORT).show()
-
-            //}
-        })
-    }
-
-    private fun resetAlert() {
-        til_username.setError(null)
-        til_password.setError(null)
-        til_namaLengkap.setError(null)
-        til_noHP.setError(null)
-        til_email.setError(null)
-        til_tglLahir.setError(null)
-    }
-
-    private fun btnBackLoginListener() {
-        reg_imgBack.setOnClickListener {
+        binding.regImgBack.setOnClickListener {
             val moveHome = Intent(this, MainActivity::class.java)
             startActivity(moveHome)
         }
+
+        btnRegisterListener(binding)
+
+
     }
 
-    private fun updateLable(myCalendar: Calendar) {
-        val myFormat = "dd-MM-yyyy"
-        val sdf = SimpleDateFormat(myFormat, Locale.UK )
-        tiet_tglLahir.setText(sdf.format(myCalendar.time))
+     fun btnRegisterListener(binding: ActivityRegisterBinding) {
+
+
+         binding.regBtnRegister.setOnClickListener(View.OnClickListener {
+            var checkRegister = false
+            resetAlert(binding)
+             val username =  binding.tilNamaLengkap.getEditText()?.getText().toString()
+             val password: String =   binding.tilPassword.getEditText()?.getText().toString()
+             val nama: String = binding.tilUsername.getEditText()?.getText().toString()
+             val noHp: String = binding.tilNoHP.getEditText()?.getText().toString()
+             val email: String = binding.tilEmail.getEditText()?.getText().toString()
+             val tglLahir: String = binding.tilTglLahir.getEditText()?.getText().toString()
+
+             val mBundle = Bundle()
+
+             mBundle.putString("username", username)
+             mBundle.putString("password", password)
+
+             if (username.isEmpty()){
+                 binding.tilUsername.setError("Username must be filled ")
+                 checkRegister = false
+             }
+
+             if (password.isEmpty()){
+                 binding.tilPassword.setError("Password must be filled ")
+                 checkRegister = false
+             }
+
+             if (nama.isEmpty()){
+                 binding.tilNamaLengkap.setError("Nama must be filled ")
+                 checkRegister = false
+             }
+
+             if (noHp.isEmpty()){
+                 binding.tilNoHP.setError("Nomor HP must be filled ")
+                 checkRegister = false
+             }
+
+             if (email.isEmpty()){
+                 binding.tilEmail.setError("Email must be filled ")
+                 checkRegister = false
+             }
+
+             if (tglLahir.isEmpty()){
+                 binding.tilTglLahir.setError("Tanggal Lahir must be filled ")
+                 checkRegister = false
+             }
+
+             if(!nama.isEmpty() && !tglLahir.isEmpty() && !noHp.isEmpty() && !email.isEmpty() && !username.isEmpty() && !password.isEmpty() ){
+                 checkRegister = true
+             }
+
+             if(!checkRegister){
+                 return@OnClickListener
+
+             }else {
+//                val moveLogin = Intent(this, LoginActivity::class.java)
+
+//                val user = Users(0,username,password,nama,email,noHp,tglLahir)
+//                GlobalScope.launch(Dispatchers.IO){
+//                    usersDb.usersDao().addUsers(user)
+//
+//                }
+                 Toast.makeText(this, "Masukkan berhasil!", Toast.LENGTH_SHORT).show()
+
+             }
+
+             })
     }
+
+    private fun resetAlert(binding: ActivityRegisterBinding) {
+        binding.tilEmail.setError(null)
+        binding.tilUsername.setError(null)
+        binding.tilNoHP.setError(null)
+        binding.tilTglLahir.setError(null)
+        binding.tilNamaLengkap.setError(null)
+        binding.tilPassword.setError(null)
+    }
+
+
+    private fun updateLable(myCalendar: Calendar, binding: ActivityRegisterBinding) {
+        val myFormat = "dd-MM-yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.UK)
+        binding.tietTglLahir.setText(sdf.format(myCalendar.time))
+    }
+
+}
 
     // KODE SEBELUMNYA TANPA VIEW BINDING
 
@@ -297,5 +305,5 @@ class RegisterActivity : AppCompatActivity() {
         inputEmail.setError(null)
 
         inputTglLahir.setError(null)
-    }*/
-}
+    }
+}*/
