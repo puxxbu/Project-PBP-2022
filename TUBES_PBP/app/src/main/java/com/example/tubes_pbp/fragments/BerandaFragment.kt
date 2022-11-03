6cd517c81a3235298c1a4dc7c9a0f8ee09e02c82
puxxbu <pusswarog@gmail.com>
2022-11-03 14:50:22 +0700

    pindah lokasi button pick hotel nearby pada fragment beranda

--- TUBES_PBP/app/src/main/java/com/example/tubes_pbp/fragments/BerandaFragment.kt
+++ TUBES_PBP/app/src/main/java/com/example/tubes_pbp/fragments/BerandaFragment.kt
@@ -1,19 +1,40 @@
 package com.example.tubes_pbp.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.tubes_pbp.R
+import com.example.tubes_pbp.databinding.FragmentAkunBinding
+import com.example.tubes_pbp.databinding.FragmentBerandaBinding
+import com.example.tubes_pbp.databinding.FragmentPesananBinding
+import com.example.tubes_pbp.maps.MapActivity
 
 class BerandaFragment : Fragment(R.layout.fragment_beranda) {
+    private var _binding : FragmentBerandaBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_beranda, container, false)
+        _binding = FragmentBerandaBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+
+        val moveMap = Intent(getActivity(), MapActivity::class.java)
+
+        binding.btnMap.setOnClickListener{
+            startActivity(moveMap)
+        }
+
+
+
+
+        return rootView
     }
 
 }
--- TUBES_PBP/app/src/main/java/com/example/tubes_pbp/maps/MapActivity.kt
+++ TUBES_PBP/app/src/main/java/com/example/tubes_pbp/maps/MapActivity.kt
@@ -29,6 +29,7 @@ class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        supportActionBar?.hide()
 
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
 
