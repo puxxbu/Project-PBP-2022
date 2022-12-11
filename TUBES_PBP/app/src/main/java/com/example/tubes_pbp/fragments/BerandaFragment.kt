5d9d524c33871807e8f3b88e492661a85983ecf4
puxxbu <puxxbu@users.noreply.github.com>
2022-12-11 21:25:41 +0700

    add get jumlah pesanan + bookmark, keadaan login autopass

--- TUBES_PBP/app/src/main/java/com/example/tubes_pbp/fragments/BerandaFragment.kt
+++ TUBES_PBP/app/src/main/java/com/example/tubes_pbp/fragments/BerandaFragment.kt
@@ -1,21 +1,39 @@
 package com.example.tubes_pbp.fragments
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tubes_pbp.R
 import com.example.tubes_pbp.databinding.FragmentAkunBinding
 import com.example.tubes_pbp.databinding.FragmentBerandaBinding
 import com.example.tubes_pbp.databinding.FragmentPesananBinding
 import com.example.tubes_pbp.maps.MapActivity
+import com.example.tubes_pbp.webapi.BookmarkAdapter
+import com.example.tubes_pbp.webapi.BookmarkData
+import com.example.tubes_pbp.webapi.RClient
+import com.example.tubes_pbp.webapi.ResponseDataBookmark
+import com.example.tubes_pbp.webapi.pesananapi.PesananAdapter
+import com.example.tubes_pbp.webapi.pesananapi.PesananData
+import com.example.tubes_pbp.webapi.pesananapi.ResponseDataPesanan
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class BerandaFragment : Fragment(R.layout.fragment_beranda) {
     private var _binding : FragmentBerandaBinding? = null
     private val binding get() = _binding!!
+    private val listBookmark = ArrayList<BookmarkData>()
+    private val listPesanan = ArrayList<PesananData>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +49,9 @@ class BerandaFragment : Fragment(R.layout.fragment_beranda) {
 //            startActivity(moveMap)
 //        }
 
+        getDataBookmark()
+        getDataPesanan()
+
         binding.tmlMaps.setOnClickListener{
             startActivity(moveMap)
         }
@@ -40,5 +61,67 @@ class BerandaFragment : Fragment(R.layout.fragment_beranda) {
 
         return rootView
     }
+
+    private fun getDataBookmark() {
+        RClient.instances.getAllData().enqueue(object :
+            Callback<ResponseDataBookmark> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(
+                call: Call<ResponseDataBookmark>,
+                response: Response<ResponseDataBookmark>
+            ){
+                if (response.isSuccessful){
+                    listBookmark.clear()
+                    response.body()?.let { listBookmark.addAll(it.data) }
+                    Log.d(ContentValues.TAG,context.toString() +"CONTEXT")
+
+                    if ( isAdded() ){
+                        with(binding){
+                            tvMarks.setText(listBookmark.size.toString())
+                        }
+
+                    }
+
+
+
+                }
+            }
+            override fun onFailure(call: Call<ResponseDataBookmark>, t: Throwable) {
+                Log.d(ContentValues.TAG, "GAGAL")
+
+            }
+        }
+        )
+    }
+
+    private fun getDataPesanan() {
+        com.example.tubes_pbp.webapi.pesananapi.RClient.instances.getAllData().enqueue(object : Callback<ResponseDataPesanan> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(
+                call: Call<ResponseDataPesanan>,
+                response: Response<ResponseDataPesanan>
+            ){
+                if (response.isSuccessful){
+                    listPesanan.clear()
+                    response.body()?.let { listPesanan.addAll(it.data) }
+
+                    if ( isAdded() ){
+                        with(binding){
+                            tvPesanan.setText(listPesanan.size.toString())
+                        }
+                    }
+
+
+
+                }
+            }
+            override fun onFailure(call: Call<ResponseDataPesanan>, t: Throwable) {
+                Log.d(ContentValues.TAG, "GAGAL")
+
+            }
+        }
+        )
+    }
+
 
 }
--- TUBES_PBP/app/src/main/java/com/example/tubes_pbp/fragments/BookmarkFragment.kt
+++ TUBES_PBP/app/src/main/java/com/example/tubes_pbp/fragments/BookmarkFragment.kt
@@ -1,62 +1,4 @@
 package com.example.tubes_pbp.fragments
-//
-//import android.os.Bundle
-//import androidx.fragment.app.Fragment
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import androidx.recyclerview.widget.LinearLayoutManager
-//import androidx.recyclerview.widget.RecyclerView
-//import androidx.appcompat.app.AppCompatActivity
-//import com.example.tubes_pbp.MyAdapter
-//import com.example.tubes_pbp.R
-//import com.example.tubes_pbp.entity.Hotel
-//
-//class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
-//
-//    lateinit var adapter: MyAdapter
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//       return inflater.inflate(R.layout.fragment_bookmark, container, false)
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        val layoutManager = LinearLayoutManager(context)
-//        val rvHotel : RecyclerView = view.findViewById(R.id.hotelView)
-//        init()
-//        rvHotel.layoutManager = layoutManager
-//        rvHotel.adapter = adapter
-//
-//    }
-//
-//
-//    private fun init() {
-//
-//
-//        var data = ArrayList<Hotel>()
-//        data.add(Hotel(R.drawable.hotel7, "Four Seasons Resort Bali", "Jimbaran Bay Jimbaran"))
-//        data.add(Hotel(R.drawable.hotel8, "Jumana Bali Ungasan", "Jl. Melasti, Banjar Kelod Ungasan Uluwatu"))
-//        data.add(Hotel(R.drawable.hotel9, "Padma Resort Legian", "Jln. Padma No.1 PO BOX 1107 TBB Legian"))
-//        data.add(Hotel(R.drawable.hotel10, "The St. Regis Bali Resort", "Kawasan Pariwisata Nusa Dua Lot S6 PO BOX 44 Nusa Dua"))
-//        data.add(Hotel(R.drawable.hotel11, "Fairmont Jakarta", "Jl. Asia Afrika No. 8, Gelora Bung Karno Jakarta"))
-//        data.add(Hotel(R.drawable.hotel12, "Hotel Borobudur Jakarta", "Jalan Lapangan Benteng Selatan No. 1 Jakarta"))
-//        data.add(Hotel(R.drawable.hotel3, "Grand Rohan Yogyakarta", "Jl. Suryotomo No. 31 Yogyakarta"))
-//        data.add(Hotel(R.drawable.hotel4, "Sheraton Mustika Yogyakarta Resort and Spa", "Jl. Laksda Adisucipto Km.8,7 Yogyakarta"))
-//        data.add(Hotel(R.drawable.hotel11, "Cokro Adimojo Hotel", "Jl. Amidagla No. 31 Yogyakarta"))
-//        data.add(Hotel(R.drawable.hotel7, "Hotel Asal Nama", "Jl. Laksda Adisucipto Km.102 Yogyakarta"))
-//
-//        adapter = MyAdapter(data)
-//    }
-//
-//}
-
-
-// UNTUK MAIN ACTIVITY BOOKMARKNYA
 
 import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
@@ -88,7 +30,7 @@ class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
 
     override fun onStart() {
         super.onStart()
-        getDataMahasiswa()
+        getDataBookmark()
     }
 
 
@@ -104,12 +46,12 @@ class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
         )
 
         Log.d(TAG, "RELOAD")
-        getDataMahasiswa()
+        getDataBookmark()
 
         binding.txtCari.setOnKeyListener(View.OnKeyListener{ _, keyCode, event->
             if(keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP)
             {
-                getDataMahasiswa()
+                getDataBookmark()
                 return@OnKeyListener true
             }
             false
@@ -132,7 +74,7 @@ class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
 
 
     }
-    private fun getDataMahasiswa() {
+    private fun getDataBookmark() {
         binding.rvData.setHasFixedSize(true)
         binding.rvData.layoutManager= LinearLayoutManager(context)
         val bundle = arguments
